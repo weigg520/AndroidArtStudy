1ade1f07a7537b79b14680915bb6151f29bbd370
weigg <752139816@qq.com>
2017-12-06 22:20:24 +0800

    [完成Activity生命周期分析]

--- app/src/main/java/comt/example/a75213/myapplication/Main/MainActivity.java
+++ app/src/main/java/comt/example/a75213/myapplication/Main/MainActivity.java
@@ -22,34 +22,40 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private MainAdapter mAdapter;
     private List<MainBean> mMainList;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-
-
     }
 
     private void init() {
         main_listview = (ListView) findViewById(R.id.activity_main_list_view);
         //加载按钮数据
         mMainList = new ArrayList<>();
-        mMainList.add(getMainBean("android生命周期全面解析" , OneLiftActivity.class));
+        initData();
         //初始化适配器
-        mAdapter = new MainAdapter(this , mMainList);
+        mAdapter = new MainAdapter(this, mMainList);
         main_listview.setAdapter(mAdapter);
         main_listview.setOnItemClickListener(this);
     }
 
     /**
+     * 描述： 跳转到相应界面
+     */
+    private void initData() {
+        //生命周期
+        mMainList.add(getMainBean("android生命周期全面解析", OneLiftActivity.class));
+    }
+
+    /**
      * 描述：返回Bean
+     *
      * @param name
      * @param cls
      * @return
      */
-    private MainBean getMainBean(String name , Class<? extends Activity> cls){
+    private MainBean getMainBean(String name, Class<? extends Activity> cls) {
         MainBean bean = new MainBean();
         bean.setActivityClass(cls);
         bean.setNameItem(name);
@@ -59,6 +65,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         //跳转到相应Activity界面
-        startActivity(new Intent(MainActivity.this , mMainList.get(position).getActivityClass()));
+        startActivity(new Intent(MainActivity.this, mMainList.get(position).getActivityClass()));
     }
 }
--- app/src/main/java/comt/example/a75213/myapplication/chapter01/activity_lift/TwoLiftActivity.java
+++ app/src/main/java/comt/example/a75213/myapplication/chapter01/activity_lift/TwoLiftActivity.java
@@ -1,11 +1,8 @@
 package comt.example.a75213.myapplication.chapter01.activity_lift;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
 import comt.example.a75213.myapplication.R;
 
@@ -17,60 +14,48 @@ import comt.example.a75213.myapplication.R;
  * 3、onPause >> onStop >> onDestroy
  * 在Activity现在透明主体时不会调用onStop[完全不可见]
  */
-public class OneLiftActivity extends AppCompatActivity {
-    private static final String TAG = OneLiftActivity.class.getSimpleName();
-    private android.widget.TextView oneactivitytextview;
+public class TwoLiftActivity extends AppCompatActivity {
+    private static final String TAG  = TwoLiftActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_one_lift);
-        this.oneactivitytextview = (TextView) findViewById(R.id.one_activity_text_view);
-        oneactivitytextview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(OneLiftActivity.this , TwoLiftActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_two_lift);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e(TAG, "onStart");
+        Log.e(TAG , "onStart 2");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        //在进入第二个界面时候，必须将此方法执行完毕
-        for (int i = 0 ; i < 100000 ; i++){
-            Log.e(TAG , " " + i);
-        }
-        Log.e(TAG, "onPause");
+        Log.e(TAG , "onPause 2");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e(TAG, "onResume");
+        Log.e(TAG , "onResume 2");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e(TAG, "onStop");
+        Log.e(TAG , "onStop 2");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e(TAG, "onDestroy");
+        Log.e(TAG , "onDestroy 2");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.e(TAG, "onRestart");
+        Log.e(TAG , "onRestart 2");
     }
 }
