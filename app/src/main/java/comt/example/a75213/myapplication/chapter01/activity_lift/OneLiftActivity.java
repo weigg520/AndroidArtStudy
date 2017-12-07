95398c5e20aa032034b6d7de4fdb30d7c9a17fd4
WeiZhenzhou <752139816@qq.com>
2017-12-07 09:52:14 +0800

    【完成第一章、Activity生命周期全面解析，包括生命周期、异常结束数据保存、防止横竖屏幕切换重新创建Activity】

--- app/src/main/java/comt/example/a75213/myapplication/chapter01/activity_lift/OneLiftActivity.java
+++ app/src/main/java/comt/example/a75213/myapplication/chapter01/activity_lift/OneLiftActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import comt.example.a75213.myapplication.R;
@@ -20,16 +21,18 @@ import comt.example.a75213.myapplication.R;
 public class OneLiftActivity extends AppCompatActivity {
     private static final String TAG = OneLiftActivity.class.getSimpleName();
     private android.widget.TextView oneactivitytextview;
+    private android.widget.EditText testonsave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_lift);
+        this.testonsave = (EditText) findViewById(R.id.test_on_save);
         this.oneactivitytextview = (TextView) findViewById(R.id.one_activity_text_view);
         oneactivitytextview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(OneLiftActivity.this , TwoLiftActivity.class));
+                startActivity(new Intent(OneLiftActivity.this, TwoLiftActivity.class));
             }
         });
     }
@@ -44,9 +47,6 @@ public class OneLiftActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         //在进入第二个界面时候，必须将此方法执行完毕
-        for (int i = 0 ; i < 100000 ; i++){
-            Log.e(TAG , " " + i);
-        }
         Log.e(TAG, "onPause");
     }
 
@@ -73,4 +73,40 @@ public class OneLiftActivity extends AppCompatActivity {
         super.onRestart();
         Log.e(TAG, "onRestart");
     }
+
+    /***********************************************************************************************************************
+     * 只有在Activity异常终止时调用
+     * 在屏幕发生横竖屏切换时
+     * 可以设置configChange="orientation" 来防止Activity重新创建
+     * 当minSdkVersion 或者targetSdkVersion 有一个 Api > 13 时，需要设置screenSize才能生效
+     */
+
+
+    /**
+     * 数据保存
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("extra_test", testonsave.getText().toString() + "数据保存恢复");
+    }
+
+    /**
+     * 数据恢复
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        testonsave.setText(savedInstanceState.getString("extra_test"));
+    }
+    //***********************************************************************************************************************
+
+    /**
+     * 内存不足杀死Activity优先级
+     * 1、前台Activity ------正在和用户交互的优先级最高
+     * 2、可见但是非前台Activity -------如Activity弹出对话框、导致Activity无法与用户直接进行交互
+     * 3、后台Activity ------已经被暂停【onStop】的Activity，优先级最低
+     */
 }
